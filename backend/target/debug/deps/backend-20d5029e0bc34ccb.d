5b17d5a2eb2c7b38f09d5b65d0f6ccce34736541
vvalntnv <vvalntnv@gmail.com>
2024-02-24 21:38:38 +0200

    Initialized rust

---
+++ backend/target/debug/deps/backend-20d5029e0bc34ccb.d
@@ -0,0 +1,5 @@
+/devspace/kolite/backend/target/debug/deps/libbackend-20d5029e0bc34ccb.rmeta: src/main.rs
+
+/devspace/kolite/backend/target/debug/deps/backend-20d5029e0bc34ccb.d: src/main.rs
+
+src/main.rs:
