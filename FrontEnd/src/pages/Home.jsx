aaba488884f1a78487a1768f3c65d77f29b0cd84
DenisHristov12 <48285397+DenisHristov12@users.noreply.github.com>
2024-03-27 23:03:08 +0200

    finished styling the filter

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -111,27 +111,7 @@ const cars = [
   },
 ];
 
-// const API_KEY = '	047dbefb-ec74-4648-baeb-bfd4d084f79d';
-
 function Home() {
-  // useEffect(function () {
-  //   async function fetchData() {
-  //     // const res = await fetch(
-  //     //   `https://mc-api.marketcheck.com/v2/search/car/active?api_key=${API_KEY}&year=2015&make=ford&include_relevant_links=true`
-  //     // );
-
-  //     const res = await fetch(
-  //       `https://mc-api.marketcheck.com/v2/listing/car/WB533CF04RCR56196-185b4c31-5cb6?api_key=${API_KEY}`
-  //     );
-
-  //     const data = await res.json();
-
-  //     console.log(data);
-  //   }
-
-  //   fetchData();
-  // }, []);
-
   return (
     <>
       <SearchBy />
---
+++ FrontEnd/src/ui/Sort.jsx
@@ -0,0 +1,71 @@
+import { BiSortAlt2 } from 'react-icons/bi';
+import { Button, SortButton } from './ButtonNav';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useOutsideClick } from '../hooks/useOutsideClick';
+
+const SortDiv = styled.div`
+  display: flex;
+  justify-content: center;
+
+  height: 20rem;
+  width: 20rem;
+
+  position: fixed;
+  top: 8%;
+  right: 0;
+
+  background-color: var(--black);
+
+  z-index: 9999;
+`;
+
+const Ul = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+function Sort() {
+  const [showSort, setShowSort] = useState(false);
+
+  function close() {
+    setShowSort(false);
+  }
+
+  function handleClick(e) {
+    e.stopPropagation(false);
+    setShowSort((sort) => !sort);
+  }
+
+  const ref = useOutsideClick(close, false);
+  return (
+    <>
+      <Button onClick={handleClick}>
+        <BiSortAlt2 />
+      </Button>
+
+      {showSort && (
+        <SortDiv ref={ref}>
+          <Ul>
+            <li>
+              <SortButton>Highest price</SortButton>
+            </li>
+            <li>
+              <SortButton>Lowest price</SortButton>
+            </li>
+            <li>
+              <SortButton>Newest(year)</SortButton>
+            </li>
+            <li>
+              <SortButton>Oldest(year)</SortButton>
+            </li>
+          </Ul>
+        </SortDiv>
+      )}
+    </>
+  );
+}
+
+export default Sort;
